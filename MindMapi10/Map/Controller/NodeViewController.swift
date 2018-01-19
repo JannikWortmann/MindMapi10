bda21162ed8c6966bf7ae8c6c8ab27dd7237fd69
sabir012 <sabir.alizadeh@gmail.com>
2018-01-19 23:02:04 +0100

    Implemented notes popup view.

--- MindMapi10/Map/Controller/NodeViewController.swift
+++ MindMapi10/Map/Controller/NodeViewController.swift
@@ -16,15 +16,27 @@ class NodeViewController: UIViewController {
     var mainNodeTitle = String()
     var shouldCreateMindMap = Bool()
     
+    // GRAPH MODEL
     var nodes = [NodeCustomView]()
     var nodesRelationMap: [[Bool]] = Array(repeating: Array(repeating: false, count: 100), count: 100)
     
     var edgeFromNode = NodeCustomView()
     var edgeToNode = NodeCustomView()
+    //
+    
+    @IBOutlet weak var visualEffect: UIVisualEffectView!
+    var effect:UIVisualEffect!
+    
+    // MARK: - Notes Sub View
+    @IBOutlet var notesSubView: UIView!
+    @IBOutlet weak var notesSubViewTitle: UILabel!
+    @IBOutlet weak var txtNotesInSubView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.settings()
+        
         if shouldCreateMindMap {
             let node = Node(width: 200,
                             height: 110,
@@ -45,6 +57,15 @@ class NodeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func settings(){
+        effect = visualEffect.effect
+        visualEffect.effect = nil
+        
+        self.notesSubView.layer.borderWidth = 1
+        self.notesSubView.layer.cornerRadius = 5
+        self.notesSubView.layer.borderColor = UIColor.blue.cgColor
+    }
+    
     @IBAction func btnAddNode(_ sender: Any) {
         let node = Node(width: 200,
                         height: 110,
@@ -54,92 +75,9 @@ class NodeViewController: UIViewController {
     }
     
     
-    /*func drawGraph(height:CGFloat, width:CGFloat,_ view: UIView){
-        //ROOT NODE
-        var cursorX = self.view.frame.size.width/2
-        var cursorY = self.view.frame.size.height/2
-        
-        let nodeHeight:CGFloat = height
-        let nodeWidth:CGFloat = width
-        //parentNode left-top point coordinates
-        let startingPointX = self.view.frame.size.width/2 - (nodeWidth/2)
-        let startingPointY = self.view.frame.size.height/2 - (nodeHeight/2)
-        //
-        
-        let parentNode = NodeCustomView(frame: CGRect(x:startingPointX,y:startingPointY, width:nodeWidth, height:nodeHeight))
-        parentNode.mainLabel.text = "HCI Augmented Reality"
-        view.addSubview(parentNode)
-        
-        // PREPARE for CHILDREN
-        var childNodesCountRightOrLeft:CGFloat = 4
-        let childNodesPanelHeight = self.view.frame.size.height*3/4 //startingPointY + parentNode.bounds.height/2
-        let childNodesPanelStartY = (self.view.frame.size.height - childNodesPanelHeight) * 0.5
-        let childNodesDistanceFromParent = (startingPointX + parentNode.bounds.width) * 0.3
-        var childNodesDistanceBetweenEach = (childNodesPanelHeight - childNodesCountRightOrLeft * nodeHeight) / (childNodesCountRightOrLeft - 1)
-        
-        //BUILD RIGHT CHILDREN
-        cursorY = childNodesPanelStartY
-        cursorX = startingPointX + parentNode.bounds.width + childNodesDistanceFromParent
-        for index in 1...Int(childNodesCountRightOrLeft) {
-            let node = NodeCustomView(frame: CGRect(x:cursorX,y:cursorY, width:nodeWidth, height:nodeHeight))
-            node.mainLabel.text = "Reference " + String(index)
-            view.addSubview(node)
-            
-            //DRAW LINKS
-            var from = CGPoint(x: startingPointX + nodeWidth, y: startingPointY + nodeHeight/2)
-            var to = CGPoint(x: startingPointX + nodeWidth + childNodesDistanceFromParent/4, y: startingPointY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,tailWidth: 2, headWidth: 0, headLength: 0, view: self.view)
-            
-            from = to
-            to = CGPoint(x: startingPointX + nodeWidth + childNodesDistanceFromParent/4, y: cursorY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,
-                                   tailWidth: 2, headWidth: 0, headLength: 0, view: self.view)
-            
-            from = to
-            to = CGPoint(x: startingPointX + nodeWidth + childNodesDistanceFromParent, y: cursorY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,
-                                   tailWidth: 2, headWidth: 6, headLength: 8, view: self.view)
-            //PUT LABEL ON LINK
-            self.putLabelOnScreen(text: "Background",x: from.x + 2,y: from.y,view: view)
-            //
-            
-            cursorY += (childNodesDistanceBetweenEach + nodeHeight)
-        }
-        
-        childNodesCountRightOrLeft = 5
-        childNodesDistanceBetweenEach = (childNodesPanelHeight - childNodesCountRightOrLeft * nodeHeight) / (childNodesCountRightOrLeft - 1)
-        
-        //BUILD LEFT CHILDREN
-        cursorY = childNodesPanelStartY
-        cursorX = startingPointX - childNodesDistanceFromParent - nodeWidth
-        for index in 1...Int(childNodesCountRightOrLeft) {
-            let node = NodeCustomView(frame: CGRect(x:cursorX,y:cursorY, width:nodeWidth, height:nodeHeight))
-            node.mainLabel.text = "Reference " + String(index)
-            view.addSubview(node)
-            
-            //DRAW LINKS
-            var from = CGPoint(x: startingPointX, y: startingPointY + nodeHeight/2)
-            var to = CGPoint(x: startingPointX - childNodesDistanceFromParent/4, y: startingPointY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,tailWidth: 2, headWidth: 0, headLength: 0, view: self.view)
-            
-            from = to
-            to = CGPoint(x: startingPointX - childNodesDistanceFromParent/4, y: cursorY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,
-                                   tailWidth: 2, headWidth: 0, headLength: 0, view: self.view)
-            
-            from = to
-            to = CGPoint(x: startingPointX - childNodesDistanceFromParent, y: cursorY + nodeHeight/2)
-            UIBezierPath.drawArrow(from: from, to: to,
-                                   tailWidth: 2, headWidth: 6, headLength: 8, view: self.view)
-            
-            //PUT LABEL ON LINK
-            self.putLabelOnScreen(text: "Background",x: to.x + 2,y: to.y,view: view)
-            //
-            //
-            
-            cursorY += (childNodesDistanceBetweenEach + nodeHeight)
-        }
-    } */
+    @IBAction func btnCloseNotes(_ sender: Any) {
+        animateOut()
+    }
     
     func putLabelOnScreen(text:String, x: CGFloat, y:CGFloat, angle:CGFloat)-> UILabel{
         let label = UILabel(frame: CGRect(x: x, y: y, width: 120, height: 30))
@@ -189,6 +127,17 @@ class NodeViewController: UIViewController {
                 }
             }
         }
+    }
+
+    //TODO
+    @objc func popupNotes(_ sender: UIButton){
+        let node = nodes[sender.tag]
+        
+        txtNotesInSubView.text = "1. Custome Note."//"- " + nodes.joined(separator: "\n- ")
+        notesSubViewTitle.text = node.lblTitle.text
+        self.view.bringSubview(toFront: self.visualEffect)
+        //txtNotesInSubView.sizeToFit()
+        animateIn()
     }
     
     @objc func panGestureRecognizer(_ sender: UIPanGestureRecognizer) {
@@ -249,21 +198,6 @@ class NodeViewController: UIViewController {
         self.view.bringSubview(toFront: label)
     }
     
-    private func calculateArrowAngle(_ point1: CGPoint,_ point2: CGPoint)->CGFloat{
-        let hipotenuz = self.distance(point1, point2)
-        let cater = self.distance(point1, CGPoint(x:point2.x, y:point1.y))
-        let angle = acos(Double(cater/hipotenuz)) * 180 / .pi
-        print(angle)
-        
-        return CGFloat(angle)
-    }
-    
-    private func distance(_ a: CGPoint, _ b: CGPoint) -> CGFloat {
-        let xDist = a.x - b.x
-        let yDist = a.y - b.y
-        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
-    }
-    
     private func initNode(nodeInfo: Node, frame: CGRect)->NodeCustomView{
         let node = NodeCustomView(frame: frame)
         
@@ -288,6 +222,7 @@ class NodeViewController: UIViewController {
         
         node.btnIncomeEdge.addTarget(self, action: #selector(NodeViewController.edgeToIncomeNodeAction(_:)), for: .touchUpInside)
         node.btnOutgoingEdge.addTarget(self, action: #selector(NodeViewController.edgeFromOutgoingNodeAction(_:)), for: .touchUpInside)
+        node.btnNotes.addTarget(self, action: #selector(NodeViewController.popupNotes(_:)), for: .touchUpInside)
         
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(NodeViewController.panGestureRecognizer(_:)))
         node.isUserInteractionEnabled = true
@@ -295,8 +230,6 @@ class NodeViewController: UIViewController {
         
         return node
     }
-
-
     /*
     // MARK: - Navigation
 
@@ -306,5 +239,49 @@ class NodeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
+extension NodeViewController {
+    private func calculateArrowAngle(_ point1: CGPoint,_ point2: CGPoint)->CGFloat{
+        let hipotenuz = self.distance(point1, point2)
+        let cater = self.distance(point1, CGPoint(x:point2.x, y:point1.y))
+        let angle = acos(Double(cater/hipotenuz)) * 180 / .pi
+        print(angle)
+        
+        return CGFloat(angle)
+    }
+    
+    private func distance(_ a: CGPoint, _ b: CGPoint) -> CGFloat {
+        let xDist = a.x - b.x
+        let yDist = a.y - b.y
+        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
+    }
+}
 
+extension NodeViewController {
+    func animateIn(){
+        self.view.addSubview(notesSubView)
+        notesSubView.center = self.view.center
+        
+        notesSubView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        notesSubView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4){
+            self.visualEffect.effect = self.effect
+            self.visualEffect.isUserInteractionEnabled = true
+            self.notesSubView.alpha = 1
+            self.notesSubView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func animateOut(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.notesSubView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.notesSubView.alpha = 0
+            
+            self.visualEffect.effect = nil
+            self.visualEffect.isUserInteractionEnabled = false
+        }) { (success:Bool) in
+            self.notesSubView.removeFromSuperview()
+        }
+    }
 }
