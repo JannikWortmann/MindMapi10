58a9973d6caf6829a0763558884b5eb592d3375c
sabir012 <sabir.alizadeh@gmail.com>
2018-01-19 14:53:16 +0100

    Initial window added

---
+++ MindMapi10/Map/UIGraphDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  UIGraphDelegate.swift
+//  MindMapi10
+//
+//  Created by Sabir Alizada on 1/17/18.
+//  Copyright © 2018 Halt. All rights reserved.
+//
+
+import Foundation
+
+protocol UIGraphDelegate{
+    
+}
